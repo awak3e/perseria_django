77b89db7a1ed7d93f7ed2617e59fee412fa4e2c3
George Quarton <georgequarton@hotmail.com>
2013-01-31 23:50:12 +0000

    updating css

--- perseria_django/home/models.py
+++ perseria_django/home/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Mechanics(models.Model):
-    name = models.CharField(max_length=20)
+class Mechanic(models.Model):
     user = models.ForeignKey(User)
     
 class Cover(models.Model):
     name = models.CharField(max_length=20)
+    
+class Job(models.Model):
+    user = models.ForeignKey(User)
+    
