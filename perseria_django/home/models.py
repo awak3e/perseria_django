6814294aa6d2d8d28235ed7c3ab53c11b4320556
George Quarton <georgequarton@hotmail.com>
2013-01-31 23:36:10 +0000

    added mechanics

--- perseria_django/home/models.py
+++ perseria_django/home/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Mechanics(models.Model):
+    name = models.CharField(max_length=20)
     user = models.ForeignKey(User)
