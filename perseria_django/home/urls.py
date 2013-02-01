9a341e8578fa81a1d084a98b67ebab1a62c678b6
unknown <GeorgeQuarton@.(none)>
2013-02-01 22:28:54 +0000

    c

--- perseria_django/home/urls.py
+++ perseria_django/home/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
+from home.api import UserResource
+
+user_resource = UserResource()
 
 urlpatterns = patterns('',
     # Examples:
@@ -16,4 +19,5 @@ urlpatterns = patterns('',
     url(r'^cover/', 'home.views.cover'),
     url(r'^signout/', 'home.views.signout'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(user_resource.urls))
 )
