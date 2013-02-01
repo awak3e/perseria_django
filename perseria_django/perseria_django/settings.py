68823d0b9f47e84c627b6363459d9288d3b16d8d
unknown <GeorgeQuarton@.(none)>
2013-02-01 21:43:31 +0000

    Added tasty pie

--- perseria_django/perseria_django/settings.py
+++ perseria_django/perseria_django/settings.py
@@ -140,6 +140,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'home',
+    'tastypie',
 )
 
 # A sample logging configuration. The only tangible logging
