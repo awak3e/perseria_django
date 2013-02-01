f2ab240cc7a00fecf4d26d716979703b7260010f
George Quarton <georgequarton@hotmail.com>
2013-02-01 00:05:12 +0000

    improved models

--- perseria_django/home/models.py
+++ perseria_django/home/models.py
@@ -3,9 +3,18 @@ from django.contrib.auth.models import User
 
 class Mechanic(models.Model):
     user = models.ForeignKey(User)
+
+class Vehicle(models.Model):
+    user = models.ForeignKey(User)
+    model = models.CharField(max_length=30)
+    make = models.CharField(max_length=30)
+    registration_no = models.CharField(max_length=30)
     
 class Cover(models.Model):
-    name = models.CharField(max_length=20)
+    user = models.ForeignKey(User)
+    start_date = models.DateField() 
+    end_date = models.DateField()
+    vehicles = models.ForeignKey(Vehicle) 
     
 class Job(models.Model):
     user = models.ForeignKey(User)
