f839a62c03ea3b6d7cc75a757a093cdb3e5b954f
unknown <GeorgeQuarton@.(none)>
2013-02-01 22:48:35 +0000

    fixed user in api

--- perseria_django/home/api.py
+++ perseria_django/home/api.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 class UserResource(ModelResource):
     class Meta:
-        queryset = User.objects.get(username="awak3e")
+        queryset = User.objects.all()
         resource_name = 'user'
